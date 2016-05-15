f8e364e0d6773cfc411ab491a412f93459f85570
Charles Wentzel <charles@ramparthosting.com>
2016-05-14 22:39:20 -0400

    Passed weight test: nickel, dime, quarter

--- methods/vending-logic.js
+++ methods/vending-logic.js
@@ -1,5 +1,18 @@
 exports.checkWeight = function(coin){
-    return coin.weight;
+    var rtn = {result:false,value:false};
+    if (coin.weight == 5){
+        rtn.result = true;
+        rtn.value = 5;
+    }
+    else if (coin.weight == 2.3){
+        rtn.result = true;
+        rtn.value = 10;
+    }
+    else if (coin.weight == 5.7){
+        rtn.result = true;
+        rtn.value = 25;
+    }
+    return rtn;
 };
 
 
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -1,17 +1,39 @@
 var expect    = require("chai").expect;
 var machine = require("../methods/vending-logic.js");
 
+var nickel = {"name" : "nickel",
+    "value_dec" : "0.05",
+    "value_int" : "5",
+    "weight" : "5",
+    "diameter" : "21"};
+
+var dime = {
+    "name" : "dime",
+    "value_dec" : "0.1",
+    "value_int" : "10",
+    "weight" : "2.3",
+    "diameter" : "18"
+}
+
+var quarter = {
+    "name" : "quarter",
+    "value_dec" : "0.25",
+    "value_int" : "25",
+    "weight" : "5.7",
+    "diameter" : "24"
+}
+
 describe("Vending Machine", function() {
     describe("weigh coins", function() {
-        it("weigh nickel", function() {
-            var nickel = {"name" : "nickel",
-                "value_dec" : "0.05",
-                "value_int" : "5",
-                "weight" : "5",
-                "diameter" : "21"};
-            var result = machine.checkWeight(nickel);
+        it("weigh coins", function() {
+            var result1 = machine.checkWeight(nickel);
+            var result2 = machine.checkWeight(dime);
+            var result3 = machine.checkWeight(quarter);
 
-            expect(result).to.equal("5");
+            expect(result1).to.equal("5");
+            expect(result2).to.equal("10");
+            expect(result1).to.equal("25");
+
         });
     });
 
