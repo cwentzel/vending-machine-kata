f54247f8555ef1882dc7b224ce9d6687a211fe7b
Charles Wentzel <charles@ramparthosting.com>
2016-05-21 19:32:14 -0400

    Check coins in hold test passing

---
+++ methods/db-logic.js
@@ -0,0 +1,16 @@
+var monk = require("monk");
+var db = monk('localhost:27017/vending-machine-db');
+
+
+
+exports.checkHold = function(coin){
+    var hold = db.get('hold');
+    var rtn = false;
+
+    var query = {
+        value: coin.value_int
+    };
+    var doc = hold.findOne(query);
+    return doc;
+};
+
